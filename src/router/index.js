3bfc048b3cdcab4568bc6bb2d8e18874fb6c095a
DanielWu <i.lu.f@hotmail.com.tw>
2018-01-30 16:28:25 +0800

    Set page as a module, move code position

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/pages/HelloWorld';
+import HelloWorld from '@/modules/HelloWorld';
 
 Vue.use(Router);
 
