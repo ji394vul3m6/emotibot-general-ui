4adf2b1483485e57810ef2060da190fed1742aff
DanielWu <i.lu.f@hotmail.com.tw>
2018-02-23 10:15:46 +0800

    Handle Object.assign not support in some browser

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import store from './store';
 import messages from './i18n';
 import PopWindow from './plugins/PopWindow';
 import CustomNotification from './plugins/CustomNotification';
+import './plugins/Polyfill';
 
 Vue.config.productionTip = false;
 Vue.use(VueCookie);
---
+++ src/plugins/Polyfill.js
@@ -0,0 +1,25 @@
+// Polyfill for Object.assign if not support
+if (typeof Object.assign !== 'function') {
+  const assign = function (target, ...varArgs) { // .length of function is 2
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    const to = Object(target);
+
+    for (let index = 1; index < varArgs.length; index += 1) {
+      const nextSource = varArgs[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        Object.keys(nextSource).forEach((nextKey) => {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        });
+      }
+    }
+    return to;
+  };
+  Object.assign = assign;
+}
