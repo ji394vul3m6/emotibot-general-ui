27abe071703890d33d1b590582403540f455be8c
DanielWu <i.lu.f@hotmail.com.tw>
2018-05-12 11:54:22 +0800

    Add ability to descrypt password in entrypoint.sh and fix bugs of init

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+#decrypt password for php
+if ! [[ -z "$DECRYPTION_SERVICE" ]]; then
+    MYSQL_PASSWORD=$(/api/desEncrypt -t $ADMIN_HT_DB_PASSWORD)
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
+    export ADMIN_HT_DB_PASSWORD=$MYSQL_PASSWORD
+    
+    MYSQL_PASSWORD=$(/api/desEncrypt -t $ADMIN_HT_AUDIT_DB_PASSWORD)
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
+    export ADMIN_HT_AUDIT_DB_PASSWORD=$MYSQL_PASSWORD
+fi
+
 # add env to php-fpm config
 env | grep ADMIN_HT | sed -e "s/^ADMIN_//" | sed -e "s/^\([A-Z_a-z0-9]*\)=\"\{0,1\}\([^\"]*\)\"\{0,1\}/env[\1] = \"\2\"/g" >> /etc/php5/fpm.d/www.conf
 php-fpm -R
@@ -7,11 +22,19 @@ php-fpm -R
 # start crond
 crond -f &
 
+#decrypt password
+if ! [[ -z "$DECRYPTION_SERVICE" ]]; then
+    MYSQL_PASSWORD=$(/api/desEncrypt -t $ADMIN_AUTH_MYSQL_PASS)
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
+    export ADMIN_AUTH_MYSQL_PASS=$MYSQL_PASSWORD
+fi
 # start authentication 
 /auth/token-auth &
 
 # start api
-cd /api && ./file_init.sh && ./entrypoint.sh env.template &
+cd /api && ./files_init.sh && ./entrypoint.sh env.template &
 
 # Add tmp dir for fastcgi php usage, used for uploaded file
 mkdir /build/dist/tmp && chmod 777 /build/dist/tmp
