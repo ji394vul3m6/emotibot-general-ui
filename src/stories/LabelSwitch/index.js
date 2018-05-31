9ac0193d335ad1bcb7ae284035efd237e0805789
JoanneWang <joannewang@emotibot.com>
2018-05-31 10:19:46 +0800

    Add LabelSwitch story

---
+++ src/stories/LabelSwitch/index.js
@@ -0,0 +1,44 @@
+import { object, text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import LabelSwitch from '../../components/basic/LabelSwitch';
+
+export default [{
+  name: 'props',
+  func: () => {
+    const items = [
+      {
+        text: '1天',
+        val: '1',
+      },
+      {
+        text: '7天',
+        val: '7',
+      },
+      {
+        text: '30天',
+        val: '30',
+      },
+    ];
+    const value = text('value', '1');
+    const labelItems = object('options', items);
+    const template = '<label-switch v-model="value" :options="labelItems" @change="chooseLabel"></label-switch>';
+
+    return {
+      components: {
+        LabelSwitch,
+      },
+      data() {
+        return {
+          labelItems,
+          value,
+        };
+      },
+      methods: {
+        chooseLabel(val) {
+          action('choose label switch')(val);
+        },
+      },
+      template,
+    };
+  },
+}];
--- src/stories/index.js
+++ src/stories/index.js
@@ -4,8 +4,14 @@ import { storiesOf } from '@storybook/vue';
 import { withKnobs } from '@storybook/addon-knobs';
 
 import TextButtonStory from './TextButton';
+import LabelSwitchStory from './LabelSwitch';
 
 let textButtonStory = storiesOf('Textbutton', module).addDecorator(withKnobs);
 TextButtonStory.forEach((story) => {
   textButtonStory = textButtonStory.add(story.name, story.func);
+});
+
+let labelStory = storiesOf('LabelSwitch', module).addDecorator(withKnobs);
+LabelSwitchStory.forEach((story) => {
+  labelStory = labelStory.add(story.name, story.func);
 });
