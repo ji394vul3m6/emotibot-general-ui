f6f9448a1c936210fbb664ef41a06c54c5bacfc3
DanielWu <i.lu.f@hotmail.com.tw>
2018-06-19 11:13:41 +0800

    User text-button for default component in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,14 +6,19 @@ import Vuex from 'vuex'; // Vue plugins
 // Import your custom components.
 import Icon from '../src/components/basic/Icon';
 import Tag from '../src/components/basic/Tag';
+import Textbutton from '../src/components/basic/TextButton';
+
+// Import self plugins.
+import tooltip from '../src/plugins/tooltip';
 
 // Install Vue plugins.
 Vue.use(Vuex);
+Vue.use(tooltip);
+
+// Install self component
 Vue.component('icon', Icon);
 Vue.component('tag', Tag);
-
-// Register custom components.
-// Vue.component('my-button', Mybutton);
+Vue.component('text-button', Textbutton);
 
 function loadStories() {
   // You can require as many stories as you need.
