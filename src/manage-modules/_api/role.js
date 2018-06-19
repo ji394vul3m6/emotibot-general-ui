b15fe392949215010335923dab2f63c707c5f5fc
DanielWu <i.lu.f@hotmail.com.tw>
2018-06-19 11:01:56 +0800

    Remove redundant API

--- src/manage-modules/_api/role.js
+++ src/manage-modules/_api/role.js
@@ -1,10 +1,5 @@
 import qs from 'qs';
 
-const ROLE_URL = '/admin/v1/role';
-const ROLES_URL = '/admin/v1/roles';
-const ROLE_REGISTER_URL = '/admin/v1/role/register';
-
-// const V2_PREFIX = '/auth/v2';
 const V3_PREFIX = '/auth/v3';
 
 function getRolesURL(enterpriseID) {
@@ -57,68 +52,7 @@ function deleteEnterpriseRole(enterprise, id) {
   });
 }
 
-function deleteRole(id) {
-  return this.$reqDelete(`${ROLE_URL}/${id}`).then(rsp => rsp.data);
-}
-
-function getRole(id) {
-  return this.$reqGet(`${ROLE_URL}/${id}`).then((rsp) => {
-    const role = rsp.data.result;
-    return {
-      role_name: role.role_name,
-      role_id: role.role_id,
-      privileges: JSON.parse(role.privilege),
-    };
-  });
-}
-
-function updateRole(id, role) {
-  const options = {
-    role_name: role.name,
-    privilege: JSON.stringify(role.privileges),
-  };
-
-  return this.$reqPatch(`${ROLE_URL}/${id}`, qs.stringify(options), {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-}
-
-function getRoles() {
-  return this.$reqGet(ROLES_URL).then((rsp) => {
-    const ret = [];
-    rsp.data.result.forEach((role) => {
-      ret.push({
-        role_name: role.role_name,
-        role_id: role.role_id,
-        privileges: JSON.parse(role.privilege),
-      });
-    });
-    return ret;
-  });
-}
-
-function addRole(name, privileges) {
-  const options = {
-    role_name: name,
-    privilege: JSON.stringify(privileges),
-  };
-
-  return this.$reqPost(ROLE_REGISTER_URL, qs.stringify(options), {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-}
-
 export default {
-  getRoles,
-  getRole,
-  addRole,
-  updateRole,
-  deleteRole,
-
   getEnterpriseRoles,
   getEnterpriseRole,
   updateEnterpriseRole,
--- src/manage-modules/_api/user.js
+++ src/manage-modules/_api/user.js
@@ -1,9 +1,5 @@
 import qs from 'qs';
 
-const USER_URL = '/admin/v1/user';
-const USERS_URL = '/admin/v1/users';
-const USER_REGISTER_URL = '/admin/v1/user/register';
-
 const BF2_USER_URL = '/api/v1/bf/user';
 
 // const V2_PREFIX = '/auth/v2';
@@ -68,39 +64,7 @@ function deleteEnterpriseUser(enterprise, id) {
     .then(rsp => rsp.data.result);
 }
 
-function getUsers() {
-  return this.$reqGet(USERS_URL).then(rsp => rsp.data.result);
-}
-function getUser(id) {
-  return this.$reqGet(`${USER_URL}/${id}`).then(rsp => rsp.data.result);
-}
-function addUser(user) {
-  user.type = 1;
-  user.username = user.name;
-  return this.$reqPost(USER_REGISTER_URL, qs.stringify(user), {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-}
-function updateUser(id, user) {
-  return this.$reqPatch(`${USER_URL}/${id}`, qs.stringify(user), {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-}
-function deleteUser(id) {
-  return this.$reqDelete(`${USER_URL}/${id}`);
-}
-
 export default {
-  getUsers,
-  getUser,
-  addUser,
-  updateUser,
-  deleteUser,
-
   getEnterpriseUsers,
   getEnterpriseUser,
   updateEnterpriseUser,
