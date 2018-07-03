e8d613c40cdbfefa0705512a14953c3078055e32
tom <tomchen@emotibot.com>
2018-07-03 18:08:47 +0800

    FIX: Close TE 3.0
    Close TE 3.0

--- src/modules/TaskEngine/index.js
+++ src/modules/TaskEngine/index.js
@@ -1,5 +1,4 @@
 import TaskEngine from './TaskEngine';
-import TaskEngineV3 from './TaskEngineV3';
 import TaskEngineUpload from './TaskEngineUpload';
 
 export default {
@@ -7,7 +6,6 @@ export default {
   icon: 'menu_te',
   pages: {
     TaskEngine,
-    TaskEngineV3,
     TaskEngineUpload,
   },
 };
