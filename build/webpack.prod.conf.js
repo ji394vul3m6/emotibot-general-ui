9afd60addbbbd5afc16eda7e0b5a8e4235506cec
DanielWu <danielwu@emotibot.com>
2018-07-17 15:30:57 +0800

    FIX: Fix that version.html is not generated

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -96,6 +96,21 @@ const webpackConfig = merge(baseWebpackConfig, {
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
     }),
+    new HtmlWebpackPlugin({
+      filename: 'version.html',
+      template: 'version.html',
+      chunks: ["vendor", "manifest", "version"],
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
