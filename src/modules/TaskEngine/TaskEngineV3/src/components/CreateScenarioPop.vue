896cf99723ad8d85454f908d663e7a70965a181a
tom <tomchen@emotibot.com>
2018-07-30 16:43:21 +0800

    FIX: Error alert when create scenario without name
    Fix the way to show error alert in createing scenario without name

--- src/modules/TaskEngine/TaskEngineV3/src/components/CreateScenarioPop.vue
+++ src/modules/TaskEngine/TaskEngineV3/src/components/CreateScenarioPop.vue
@@ -2,14 +2,12 @@
 <div id="scenario-editor-pop">
   <div class="edit-title">{{$t("task_engine_v3.create_scenario_pop.label_name_the_scenario")}}</div>
   <div class="edit-data">
-    <input v-model="scenarioName" :placeholder="$t('task_engine_v3.create_scenario_pop.placeholder_enter_scenario_name')"></input>
+    <input ref="scenarioName" v-tooltip="noNameTooltip" v-model="scenarioName" :placeholder="$t('task_engine_v3.create_scenario_pop.placeholder_enter_scenario_name')"></input>
   </div>
 </div>
 </template>
 
 <script>
-import i18nUtils from '../utils/i18nUtil';
-import general from '../utils/general';
 
 export default {
   name: 'scenario-editor-pop',
@@ -25,6 +23,12 @@ export default {
     return {
       i18n: {},
       scenarioName: this.value.scenarioName,
+      noNameTooltip: {
+        msg: this.$t('task_engine_v3.error_msg.please_enter_the_scenario_name'),
+        eventOnly: true,
+        errorType: true,
+        alignLeft: true,
+      },
     };
   },
   computed: {},
@@ -32,18 +36,17 @@ export default {
   methods: {
     validate() {
       if (this.scenarioName === '') {
-        general.popErrorWindow(this,
-          this.i18n.task_engine_v3.error_msg.please_enter_the_scenario_name,
-          '',
-        );
+        this.$refs.scenarioName.dispatchEvent(new Event('tooltip-reload'));
+        this.$refs.scenarioName.dispatchEvent(new Event('tooltip-show'));
+        this.$refs.scenarioName.focus();
       } else {
         this.$emit('validateSuccess', this.scenarioName);
       }
     },
   },
-  beforeMount() {},
+  beforeMount() {
+  },
   mounted() {
-    this.i18n = i18nUtils.getLocaleMsgs(this.$i18n);
     this.$on('validate', this.validate);
   },
 };
