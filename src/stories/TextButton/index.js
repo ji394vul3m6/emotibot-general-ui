766b564eeca381a2ddfb4bfe91de0aae0302eaaf
JoanneWang <joannewang@emotibot.com>
2018-07-30 14:55:54 +0800

    FEATURE: Add iconAlign option to TextButton
    Some TextButton show icon on the right hand side
    With iconAlign option, we can set iconAlign as 'left' or 'right'

--- src/stories/TextButton/index.js
+++ src/stories/TextButton/index.js
@@ -1,4 +1,4 @@
-import { text, select } from '@storybook/addon-knobs';
+import { text, select, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import TextButton from '../../components/basic/TextButton';
 
@@ -10,20 +10,28 @@ export default [
       const height = text('高度', '40px');
       const buttonTxt = text('文字', 'TextButton');
       const buttonTypes = {
-        default: '預設',
-        fill: '一般按鈕',
-        primary: '主要按鈕',
-        disable: '失效按鈕',
-        error: '錯誤按鈕',
+        default: 'default 預設',
+        fill: 'fill 一般按鈕',
+        primary: 'primary 主要按鈕',
+        disable: 'disable 失效按鈕',
+        error: 'error 錯誤按鈕',
+      };
+      const iconAlignTypes = {
+        left: 'left 靠左',
+        right: 'right 靠右',
       };
       const buttonType = select('按鈕類型', buttonTypes, 'default');
       const iconType = text('iconType', 'search');
+      const iconSize = number('iconSize', 16);
+      const iconAlign = select('iconAlign', iconAlignTypes, 'left');
       const template = `
         <text-button
           width='${width}'
           height='${height}'
           button-type='${buttonType}'
           iconType="${iconType}"
+          iconSize="${iconSize}"
+          iconAlign="${iconAlign}"
           @click="test">
           ${buttonTxt}
         </text-button>`;
