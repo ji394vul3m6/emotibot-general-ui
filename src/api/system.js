37818c95c2857631cbaf462e90dfa009fb3c6643
DanielWu <i.lu.f@hotmail.com.tw>
2018-10-12 14:24:47 +0800

    FEATURE: Decide to show IM button or not via ENV
    Use ADMIN_UI_IM_ENABLE to set IM button display or hidden.
    When the value is 0 or false, ui will hide the button.

--- src/api/system.js
+++ src/api/system.js
@@ -1,9 +1,14 @@
 const VERSION_URL = '/api/v1/ui/versions';
+const ENV_PATH = '/api/v1/ui/envs';
 
 function getVersionInfo() {
   return this.$reqGet(VERSION_URL).then(rsp => rsp.data.result);
 }
+function getEnv() {
+  return this.$reqGet(ENV_PATH).then(rsp => rsp.data.result);
+}
 
 export default {
   getVersionInfo,
+  getEnv,
 };
