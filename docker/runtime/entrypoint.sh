c46b5c69fade602f72ec97ae543e2bbbc9a7befa
DeanSu <deansu@emotibot.com>
2018-10-17 10:41:03 +0800

    FIX: update CI Flow
    - fix crontab flag(alpine: -f, centos: -n)
    - use harbor.emotibot.com
    - Add jenkinsfile & send2Slack.sh
    - add project name for harbor compatibility
    - disable yum fastestmirror
    - use latest token auth
    - Add latest config & entrypoint
    - fix test case
    - use password stdin for docker login

--- docker/runtime/entrypoint.sh
+++ docker/runtime/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
+export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
+
 # start crond
-crond -f &
+crond -n &
 
 #decrypt password
 if ! [[ -z "$DECRYPTION_SERVICE" ]]; then
@@ -16,16 +18,9 @@ fi
 /auth/token-auth &
 
 # start api
-cd /api && ./files_init.sh && ./entrypoint.sh env.template &
+cd /api && ./files_init.sh && ./entrypoint.sh &
 
 cd /etc/nginx
 rm nginx.conf
-while read line
-do 
-  echo $line \
-    | sed -e "s/\${ADMIN_STAT_SERVER_URL}/$ADMIN_STAT_SERVER_URL/g" \
-    | sed -e "s/\${ADMIN_ELK_STAT_SERVER_URL}/$ADMIN_ELK_STAT_SERVER_URL/g" \
-    | sed -e "s/\${ADMIN_IM_SERVER_URL}/$ADMIN_IM_SERVER_URL/g" \
-    | sed -e "s/\${ADMIN_BF_SERVER_URL}/$ADMIN_BF_SERVER_URL/g" >> nginx.conf
-done < nginx.conf.template
+envsubst '$$NAMESERVER$$ADMIN_BF_SERVER_URL$$ADMIN_IM_SERVER_URL$$ADMIN_STAT_SERVER_URL$$ADMIN_ELK_STAT_SERVER_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g "daemon off;"
