75d86fcdaa866581ea7b8bac2192b343739d3706
DanielWu <i.lu.f@hotmail.com.tw>
2018-12-10 18:33:32 +0800

    FEATURE: Use supervisord

---
+++ docker/runtime/run_nginx.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /etc/nginx
+rm nginx.conf
+envsubst '
+$$NAMESERVER
+$$ADMIN_BF_SERVER_URL
+$$ADMIN_IM_SERVER_URL
+$$ADMIN_IM_MSG_KF_URL
+$$ADMIN_IM_WEB_AGENT
+$$ADMIN_STAT_SERVER_URL
+$$ADMIN_ELK_STAT_SERVER_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+nginx -g "daemon off;"
