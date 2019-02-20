f10342dc25f33ef58a5daba4f91702a0b4a89c55
Chester <chester.yw.chu@gmail.com>
2019-02-20 17:08:09 +0800

    FEATURE: remove old pages

--- src/modules/IntentEngine/index.js
+++ src/modules/IntentEngine/index.js
@@ -1,13 +1,13 @@
 import IntentManage from './IntentManage';
-import IntentManage1 from './IntentManage.1';
-import IntentTest from './IntentTest';
+// import IntentManage1 from './IntentManage.1';
+// import IntentTest from './IntentTest';
 
 export default {
   displayNameKey: 'intent_engine',
   icon: 'menu_intent',
   pages: {
     IntentManage,
-    IntentManage1,
-    IntentTest,
+    // IntentManage1,
+    // IntentTest,
   },
 };
