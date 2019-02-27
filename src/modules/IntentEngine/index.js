cc1de9bccfcf0cd5b4fa3274f0f048001d50e2b6
Chester <chester.yw.chu@gmail.com>
2019-02-27 16:45:48 +0800

    REFACTOR: remove unused component

--- src/modules/IntentEngine/index.js
+++ src/modules/IntentEngine/index.js
@@ -1,13 +1,9 @@
 import IntentManage from './IntentManage';
-// import IntentManage1 from './IntentManage.1';
-// import IntentTest from './IntentTest';
 
 export default {
   displayNameKey: 'intent_engine',
   icon: 'menu_intent',
   pages: {
     IntentManage,
-    // IntentManage1,
-    // IntentTest,
   },
 };
